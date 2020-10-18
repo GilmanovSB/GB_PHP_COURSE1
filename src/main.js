bf8a590259f365e266f5dd37639d6edb9f452d58
Sergey <gilmanov10@outlook.com>
2020-10-18 22:08:27 +0300

    homework 3 ex. complite

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-const parrent = document.querySelector('body');
+const parrent = document.querySelector('.wrapper');
+const content = document.querySelector('.modal');
 parrent.addEventListener('click', (e) => {
-    if(e.target.nodeName == 'IMG'){
-        const modalWin = document.createElement('DIV');
-        const el = e.target;
-        modalWin.classList.add('modal');
-        parrent.appendChild(modalWin);
+    if(e.target.nodeName == 'IMG' && e.target.parentElement.className == 'wrapper' || e.target.className == 'close'){
+        if(e.target.className == 'close'){
+            parrent.removeChild(document.querySelector('.modal'));
+            console.log(e);
+        } else {
+            parrent.innerHTML += `<div class = 'modal'>${e.target.outerHTML}<button class = 'close'>Close</button></div>`;
+        }
     }}
 );
